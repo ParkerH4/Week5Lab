4fe6df10cd1ebb1efe01e07c58340016bbda0c6c
user <user@10.0.0.176>
2022-10-02 23:00:44 -0600

    Servlets completed. Application is running well and logging in but not out.

--- src/java/servlets/HomeServlet.java
+++ src/java/servlets/HomeServlet.java
@@ -19,7 +19,15 @@ public class HomeServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
 
-      
+      HttpSession session = request.getSession();
+
+        if (session.getAttribute("username") != null) {
+            
+            getServletContext().getRequestDispatcher("/WEB-INF/home.jsp").forward(request, response);
+            
+        } else {
+            response.sendRedirect("login");
+        }
 
     }
 
