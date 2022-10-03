6ee4cd8c9910e3a3f33da7995d9d71e1deffdfa8
user <user@10.0.0.176>
2022-10-02 22:06:03 -0600

    Java classes complete. setup and coded the logic for the accountservice class and setup user.java

--- src/java/models/AccountService.java
+++ src/java/models/AccountService.java
@@ -1,14 +1,18 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package models;
+
+import java.io.Serializable;
 
 /**
  *
  * @author user
  */
-public class AccountService {
+public class AccountService implements Serializable {
     
+    public User login(String username, String password) {
+        if ((username.equals("abe") || username.equals("barb")) && password.equals("password")) {
+            return new User (username, null);
+        } else 
+      return null;
+    }
 }
